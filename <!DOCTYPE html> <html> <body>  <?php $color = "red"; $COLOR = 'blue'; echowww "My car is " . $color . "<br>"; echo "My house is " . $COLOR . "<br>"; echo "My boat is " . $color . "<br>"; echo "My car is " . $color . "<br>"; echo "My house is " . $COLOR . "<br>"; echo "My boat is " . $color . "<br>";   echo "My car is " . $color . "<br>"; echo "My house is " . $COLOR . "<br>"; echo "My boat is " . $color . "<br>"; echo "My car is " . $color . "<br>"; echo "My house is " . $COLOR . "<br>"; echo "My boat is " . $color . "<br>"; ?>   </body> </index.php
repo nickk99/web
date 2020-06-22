266f3a998013d0fc8b1f745ab2f3679e90af4191
Naveen Satwani <64723739+nickk99@users.noreply.github.com>
2020-06-22 14:22:36 +0530

    Create index.php

---
+++ <!DOCTYPE html> <html> <body>  <?php $color = "red"; $COLOR = 'blue'; echowww "My car is " . $color . "<br>"; echo "My house is " . $COLOR . "<br>"; echo "My boat is " . $color . "<br>"; echo "My car is " . $color . "<br>"; echo "My house is " . $COLOR . "<br>"; echo "My boat is " . $color . "<br>";   echo "My car is " . $color . "<br>"; echo "My house is " . $COLOR . "<br>"; echo "My boat is " . $color . "<br>"; echo "My car is " . $color . "<br>"; echo "My house is " . $COLOR . "<br>"; echo "My boat is " . $color . "<br>"; ?>   </body> </index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$color = "red";
+$COLOR = 'blue';
+echowww "My car is " . $color . "<br>";
+echo "My house is " . $COLOR . "<br>";
+echo "My boat is " . $color . "<br>";
+echo "My car is " . $color . "<br>";
+echo "My house is " . $COLOR . "<br>";
+echo "My boat is " . $color . "<br>";
+
+
+echo "My car is " . $color . "<br>";
+echo "My house is " . $COLOR . "<br>";
+echo "My boat is " . $color . "<br>";
+echo "My car is " . $color . "<br>";
+echo "My house is " . $COLOR . "<br>";
+echo "My boat is " . $color . "<br>";
+?> 
+
+</body>
+</html>
